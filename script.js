5d3c7703655b2de9241935d353f5b2355f93126b
kuba-lekki <kuba.lekki97@gmail.com>
2022-01-26 18:07:08 +0100

    Create computerPlay() function

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 console.info("Script.js has been loaded.");
+
+function computerPlay() {
+    let randomNumber = Math.random();
+    console.log(randomNumber);
+    if (randomNumber < 0.33) return "rock";
+    else if (randomNumber >= 0.33 && randomNumber < 0.66) return "paper";
+    else return "scissors";
+}
