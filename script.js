7a047520ca1cb7f324051666866df5264b78dc8a
kuba-lekki <kuba.lekki97@gmail.com>
2022-01-31 18:45:49 +0100

    Create playRound() function

--- script.js
+++ script.js
@@ -7,3 +7,35 @@ function computerPlay() {
     else if (randomNumber >= 0.33 && randomNumber < 0.66) return "paper";
     else return "scissors";
 }
+
+function playRound() {
+    //ask user to choose rock, paper or scissors
+    let playerSelection = (window.prompt("Rock / Paper / Scissors: ")).toLowerCase();
+    console.log("The player has chosen: " + playerSelection);
+    if (playerSelection == "rock" || playerSelection == "paper" || playerSelection == "scissors") {
+        //use the computerPlay() function to determine the CPU's play
+        let computerSelection = computerPlay();
+        console.log("The computer has chosen: " + computerSelection);
+        //compare the players selection with the CPU's selection & output the winner
+        if (playerSelection == "rock") {
+            if (computerSelection == "rock") return "tie";
+            if (computerSelection == "paper") return "computer";
+            if (computerSelection == "scissors") return "player";
+        }
+
+        if (playerSelection == "paper") {
+            if (computerSelection == "rock") return "player";
+            if (computerSelection == "paper") return "tie";
+            if (computerSelection == "scissors") return "computer";
+        }
+
+        if (playerSelection == "scissors") {
+            if (computerSelection == "rock") return "computer";
+            if (computerSelection == "paper") return "player";
+            if (computerSelection == "scissors") return "tie";
+        }
+    } else {
+        console.log("Please enter a valid option: Rock / Paper / Scissors: ");
+        playRound();
+    }
+}
